a40d8a59d64e848526a26407e7b23cf28c4239bb
oleg <zustzero123@gmail.com>
2025-01-20 23:40:17 +0300

    fix prop type error

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,34 +1,80 @@
 <template>
   <div class="item">
     <div class="itemHeader">
-      <h2>{{ 'book name' }}</h2>
-      <button>
-        <img src="../public/edit.png" alt="" />
-      </button>
+      <div class="title">
+        <h2>{{ props.title }}</h2>
+      </div>
+      <div class="btn">
+        <button>
+          <img
+            src="../../public/edit.png"
+            alt=""
+            class="icon"
+            @click="
+              () =>
+                props.editBook({
+                  id: props.id,
+                  title: props.title,
+                  author: props.author,
+                  year: props.year,
+                  category: props.category,
+                })
+            "
+          />
+        </button>
+      </div>
     </div>
     <div class="itemInfo">
-      <p>{{ 'author' }}</p>
-      <p>{{ '2020' }}</p>
-      <p>{{ 'category' }}</p>
+      <p>{{ props.author }}</p>
+      <p>{{ props.year }}</p>
+      <p>{{ props.category }}</p>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'BookItem',
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-  },
-  setup(props) {
-    // const store = useStore()
-    // const query = ref('')
-    // const showInput = ref(false)
-  },
-}
+<script lang="ts" setup>
+import '../assets/main.css'
+import { PropType } from 'vue'
+import { IBook } from '@/types'
+const props = defineProps({
+  id: String,
+  title: String,
+  author: String,
+  year: String,
+  category: String,
+  editBook: Function as PropType<(data: IBook | void) => void>,
+})
+const emit = defineEmits(['openModal'])
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.item {
+  width: 100%;
+  height: auto;
+  background-color: rgba(245, 246, 246, 1);
+  border-radius: 16px;
+  padding: 15px;
+
+  .itemHeader {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    .title {
+      width: 90%;
+    }
+    .btn {
+      height: 100%;
+      width: 10%;
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+  .itemInfo {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    color: rgba(112, 119, 134, 1);
+  }
+}
+</style>
